b9b9ff71007c6e4c6b30bfab3293a21eb89f818c
tru0067 <33011616+tru0067@users.noreply.github.com>
2024-07-19 12:06:39 +1000

    Add error handling and tidy up code

--- PierceBugFix/PierceBugDisassembler.cs
+++ PierceBugFix/PierceBugDisassembler.cs
@@ -1,36 +1,61 @@
 ﻿using Iced.Intel;
+using UnityEngine.Assertions;
 
 namespace PierceBugFix;
 
 public class PierceBugDisassembler
 {
+    public const int INC_WE_WANT = 3;
+    public const int INC_EXPECTED = 4;
+    public const int WIDTH_EXPECTED = 3;
     public static unsafe IntPtr FindThirdInc(IntPtr methodPointer)
     {
+        IntPtr instructionIP = IntPtr.Zero;  // Return value, initialized to null.
+
+        // Set up the decoder to go through the instructions.
         StreamCodeReader streamCodeReader = new((Stream)new UnmanagedMemoryStream((byte*)methodPointer, 65536L, 65536L, (FileAccess)1));
         Decoder decoder = Decoder.Create(sizeof(void*) * 8, streamCodeReader);
         decoder.IP = (ulong)(long)methodPointer;
-
-        int incCount = 0;
         Instruction instruction = new();
         decoder.Decode(out instruction);
+
+        // We are looking for the third `Inc` instruction in this method.
+        int incCount = 0;
         while (instruction.Mnemonic != Mnemonic.Int3)
         // `Int3` is an opcode that is sometimes used to halt execution for a debugger. We
         // can be reasonably sure that it will appear after our method and never be inside
         // it.
         {
-            //Logger.Info($"{instruction.Code} {instruction} {instruction.IP:X2} {*(ulong*)(instruction.IP):X2}");
             if (instruction.Mnemonic == Mnemonic.Inc)
             {
                 ++incCount;
-                if (incCount == 3)
+                if (incCount == INC_WE_WANT)
                 {
-                    streamCodeReader.Stream.Dispose();
-                    return (IntPtr)(long)instruction.IP;
+                    instructionIP = (IntPtr)(long)instruction.IP;
+
+                    // Error handling.
+                    if ((instruction.NextIP - instruction.IP) != WIDTH_EXPECTED)
+                    {
+                        Logger.Error("PierceBugFix found an instruction with an unexpected width, this probably means the method has changed in some way and we should avoid changing it.");
+                        Environment.FailFast("PierceBugFix found an instruction with an unexpected width, this probably means the method has changed in some way and we should avoid changing it.");
+                    }
                 }
             }
             decoder.Decode(out instruction);
         }
         streamCodeReader.Stream.Dispose();
-        return IntPtr.Zero;
+
+        // Error handling.
+        if (incCount != INC_EXPECTED)
+        {
+            Logger.Error("PierceBugFix didn't find the correct number of `Inc` instructions in `BulletWeapon.Fire`, this probably means the method has changed in some way and we should avoid changing it.");
+            Environment.FailFast("PierceBugFix didn't find the correct number of `Inc` instructions in `BulletWeapon.Fire`, this probably means the method has changed in some way and we should avoid changing it.");
+        }
+        if (instructionIP == IntPtr.Zero)
+        {
+            Logger.Error("PierceBugFix found a zero instruction int pointer for our `Inc` instruction, something has gone terribly wrong.");
+            Environment.FailFast("PierceBugFix found a zero instruction int pointer for our `Inc` instruction, something has gone terribly wrong.");
+        }
+        return instructionIP;
     }
 }
--- PierceBugFix/Plugin.cs
+++ PierceBugFix/Plugin.cs
@@ -14,78 +14,37 @@ namespace PierceBugFix;
 [BepInPlugin("tru0067.PierceBugFix", "PierceBugFix", "1.0.0")]
 public class Plugin : BasePlugin
 {
+    public const int NOP = 0x90;
     public override unsafe void Load()
     {
         Logger.SetupFromInit(this.Log);
         Logger.Info("PierceBugFix is loading...");
+        // Look for `BulletWeapon.Fire`.
         INativeClassStruct classStruct = UnityVersionHandler.Wrap((Il2CppClass*) Il2CppClassPointerStore<BulletWeapon>.NativeClassPtr);
         for (int i = 0; i < classStruct.MethodCount; ++i)
         {
             INativeMethodInfoStruct methodInfoStruct = UnityVersionHandler.Wrap(classStruct.Methods[i]);
             if (Marshal.PtrToStringAnsi(methodInfoStruct.Name) == "Fire")
             {
+                // Found `BulletWeapon.Fire`, now find the address of the instruction we want to change.
                 IntPtr methodPointer = methodInfoStruct.MethodPointer;
+                IntPtr instructionIP = PierceBugDisassembler.FindThirdInc(methodPointer);
 
-
-
-
-
-
-                Logger.Info("------------------------------------------------------------------");
-                StreamCodeReader streamCodeReader = new((Stream)new UnmanagedMemoryStream((byte*)methodPointer, 65536L, 65536L, (FileAccess)1));
-                Decoder decoder = Decoder.Create(sizeof(void*) * 8, streamCodeReader);
-                decoder.IP = (ulong)(long)methodPointer;
-                Instruction instruction = new();
-                decoder.Decode(out instruction);
-                while (instruction.Mnemonic != Mnemonic.Int3)
-                {
-                    Logger.Info($"{instruction.Code} {instruction} {instruction.IP:X}");
-                    decoder.Decode(out instruction);
-                }
-                streamCodeReader.Stream.Dispose();
-                Logger.Info("------------------------------------------------------------------");
-
-
-
-
-
-                IntPtr instructionIP = PierceBugDisassembler.FindThirdInc(methodInfoStruct.MethodPointer);
-
+                // Change that instruction into `NOP`s.
                 using (new MemoryProtectionCookie(instructionIP, Kernel32.MemoryProtectionConstant.ExecuteReadWrite, new IntPtr(16)))
                 {
-                    *(byte*)(instructionIP) = 0x90;
-                    *(byte*)((ulong)instructionIP + (ulong) new IntPtr(1)) = 0x90;
-                    *(byte*)((ulong)instructionIP + (ulong) new IntPtr(2)) = 0x90;
+                    for (int j = 0; j < PierceBugDisassembler.WIDTH_EXPECTED; ++j)
+                    {
+                        *(byte*)((ulong)instructionIP + (ulong)new IntPtr(j)) = NOP;
+                    }
                 }
-
-
-
-
-
-
-
-                Logger.Info("------------------------------------------------------------------");
-                StreamCodeReader streamCodeReader2 = new((Stream)new UnmanagedMemoryStream((byte*)methodPointer, 65536L, 65536L, (FileAccess)1));
-                Decoder decoder2 = Decoder.Create(sizeof(void*) * 8, streamCodeReader2);
-                decoder2.IP = (ulong)(long)methodPointer;
-                Instruction instruction2 = new();
-                decoder2.Decode(out instruction2);
-                while (instruction2.Mnemonic != Mnemonic.Int3)
-                {
-                    Logger.Info($"{instruction2.Code} {instruction2} {instruction2.IP:X}");
-                    decoder2.Decode(out instruction2);
-                }
-                streamCodeReader2.Stream.Dispose();
-                Logger.Info("------------------------------------------------------------------");
-
-
 
                 // Done!
                 Logger.Info("PierceBugFix is loaded!");
                 return;
             }
         }
-        Logger.Error("PierceBugFix failed to find BulletWeapon.Fire");
-        Environment.FailFast("PierceBugFix failed to find BulletWeapon.Fire");
+        Logger.Error("PierceBugFix failed to find `BulletWeapon.Fire`");
+        Environment.FailFast("PierceBugFix failed to find `BulletWeapon.Fire`");
     }
 }
