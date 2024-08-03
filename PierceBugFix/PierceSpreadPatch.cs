1750e6450e0cecd8e7e6429b1b458f48338b4a61
Dinorush <onparnorthstar@gmail.com>
2024-08-02 22:34:55 -0400

    Fix spread application

---
+++ PierceBugFix/PierceSpreadPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using static Weapon;
+using UnityEngine;
+using System.Reflection;
+
+namespace PierceBugFix
+{
+    [HarmonyPatch]
+    internal static class PierceSpreadPatch
+    {
+        [HarmonyTargetMethod]
+        private static MethodBase FindWeaponRayFunc(Harmony harmony)
+        {
+            return AccessTools.Method(
+                typeof(Weapon),
+                nameof(Weapon.CastWeaponRay),
+                new Type[] { typeof(Transform), typeof(Weapon.WeaponHitData).MakeByRefType(), typeof(Vector3), typeof(int) }
+                );
+        }
+
+        [HarmonyPostfix]
+        private static void PostRayCallback(ref WeaponHitData weaponRayData)
+        {
+            weaponRayData.randomSpread = 0;
+            weaponRayData.angOffsetX = 0;
+            weaponRayData.angOffsetY = 0;
+        }
+    }
+}
