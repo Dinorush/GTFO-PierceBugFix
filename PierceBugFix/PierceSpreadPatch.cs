c7b2b13c68d5e8a5a8d5c100e6f6591f27dd62f8
tru0067 <33011616+tru0067@users.noreply.github.com>
2024-08-09 15:53:24 +1000

    Reinclude fix for infinite loop
    Credit to Dinorush for the fix.

--- PierceBugFix/PierceSpreadPatch.cs
+++ PierceBugFix/PierceSpreadPatch.cs
@@ -19,12 +19,15 @@ namespace PierceBugFix
         }
 
         [HarmonyPostfix]
-        private static void PostRayCallback(ref WeaponHitData weaponRayData)
+        private static void PostRayCallback(ref WeaponHitData weaponRayData, bool __result)
         {
             // Reset shotgun spread data so that shotguns don't re-evaluate spread every pierce.
             weaponRayData.randomSpread = 0;
             weaponRayData.angOffsetX = 0;
             weaponRayData.angOffsetY = 0;
+            // Avoid the potential for an infinite loop on a miss.
+            if (!__result)
+                weaponRayData.maxRayDist = 0f;
         }
     }
 }
